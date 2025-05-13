123d028440f7e8e93a76e9a52eb6473a7a31a357
manuel <manuelruizgutierrez810@gmail.com>
2025-05-13 13:18:30 +0200

    Commit manuel ruiz 4 a github

--- src/testGit/FUtil.java
+++ src/testGit/FUtil.java
@@ -1,6 +1,13 @@
 package testGit;
 import java.io.File;
 public class FUtil {
+	 /**
+     * Verifica si un archivo con el nombre especificado existe en el sistema de archivos.
+     * @author manuel
+     * @param filename El nombre del archivo. No puede estar vacío ni en blanco.
+     * @return true si el archivo existe y false si no existe.
+     * @throws IllegalArgumentException Si el nombre del archivo está vacío o tiene solo espacios.
+     */
 	public static boolean existe(String filename) throws IllegalArgumentException {
 		if(filename.isBlank()||filename.isEmpty()) {//asdfg
 			IllegalArgumentException exc=new IllegalArgumentException("ARGUMENTO INCORRECTO, NO INTRODUZCA ESPACIOS O LINEAS VACIAS");
